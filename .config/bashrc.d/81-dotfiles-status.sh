e440d534dfcf77ff9fe2520736a89f1c3d607b2b
Eli Array Minkoff <eli@earrayminkoff.tech>
2024-05-05 18:09:48 -0400

    Show dotfiles status if committed but not pushed

--- .config/bashrc.d/81-dotfiles-status.sh
+++ .config/bashrc.d/81-dotfiles-status.sh
@@ -1,2 +1,5 @@
 # print a list of changes that need to be committed to my dotfiles repo
-dotfiles status --short
+case "$(dotfiles status --porcelain --branch)" in
+    '## main...origin/main') : ;; # do nothing, already committed and pushed
+    *) dotfiles status --short --branch ;; # show brief human-readable info
+esac
