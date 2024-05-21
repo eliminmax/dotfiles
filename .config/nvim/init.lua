9196b4d4f2b9212f3c2bdce604d7dd2f1e16d448
Eli Array Minkoff <eli@earrayminkoff.tech>
2024-05-21 17:39:12 -0400

    Update nvim's init.lua for v0.10.0
    No longer set RainbowDelimiter colors based on old default colors
    
    Override new color scheme's ColorColumn

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -63,49 +63,21 @@ if nvim_version.major > 0 or nvim_version.minor >= 8 then
       end,
     })
 
-    -- @@@@@@@@@@@@@@@@@@@@@@@@@ --
-    -- @ Plugin configurations @ --
-    -- @@@@@@@@@@@@@@@@@@@@@@@@@ --
-
-    -- rainbow-delimiters.nvim
-
-    function set_rd_hl_colors(red, yellow, blue, orange, green, violet, cyan)
-        vim.cmd('highlight RainbowDelimiterRed guifg=' .. red)
-        vim.cmd('highlight RainbowDelimiterYellow guifg=' .. yellow)
-        vim.cmd('highlight RainbowDelimiterBlue guifg=' .. blue)
-        vim.cmd('highlight RainbowDelimiterOrange guifg=' .. orange)
-        vim.cmd('highlight RainbowDelimiterGreen guifg=' .. green)
-        vim.cmd('highlight RainbowDelimiterViolet guifg=' .. violet)
-        vim.cmd('highlight RainbowDelimiterCyan guifg=' .. cyan)
+    --- @@@@@@@@@@@@@@@@@ ---
+    --- @ Miscellaneous @ ---
+    --- @@@@@@@@@@@@@@@@@ ---
+    
+    -- I prefer a more intense colorcolumn - taken from Moonfly's MoonflyCranberry
+    if vim.g.colors_name == 'moonfly' then
+        vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#e65e72" })
     end
-
-    function set_rd_hl_colors_for(colorscheme, red, yellow, blue, orange, green, violet, cyan)
-        -- if it's already set to this colorscheme, then set the colors appropriately
-        if vim.g.colors_name == colorscheme then
-            set_rd_hl_colors(red, yellow, blue, orange, green, violet, cyan)
+    -- create autocmd to do the above for me
+    vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = colorscheme,
+        callback = function(ev)
+            vim.cmd('highlight ColorScheme guibg=#e65e72')
         end
-        -- create an autocmd to run when switching to colorscheme
-        vim.api.nvim_create_autocmd("ColorScheme", {
-            pattern = colorscheme,
-            callback = function(ev)
-                set_rd_hl_colors(red, yellow, blue, orange, green, violet, cyan)
-            end
-        })
-    end
-
-    -- sourcerer
-    -- most colors are defined in https://github.com/xero/sourcerer/blob/master/sourcerer.Xresources
-    -- the yellow is desaturated 50% using https://github.com/sharkdp/pastel
-    -- the orange is from mixing the desaturated yellow and the default red (also using pastel)
-    set_rd_hl_colors_for('sourcerer', '#aa4450', '#bf8c40', '#6688aa', '#b56a4a', '#719611', '#8f6f8f', '#528b8b')
-
-
-    -- vylight
-    -- colors from https://github.com/sharkdp/pastel
-    -- salmon, darkkhaki, mediumslateblue, peru, forestgreen, darkorchid, teal
-    set_rd_hl_colors_for('vylight', '#fa8072', '#bdb76b', '#7b68ee', '#cd853f', '#228b22', '#9932cc', '#008080')
-
-    -- colors from https://github.com/sharkdp/pastel
-    -- red, yellow, blue, orange, lime, violet, cyan
-    set_rd_hl_colors_for('default', '#ff0000','#ffff00','#0000ff','#ffa500','#00ff00','#ee82ee','#00ffff')
+    })
+    
+    
 end
